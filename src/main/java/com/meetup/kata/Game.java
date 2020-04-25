97d7929f2181a287d3455ac6720d5a46e1096e2b
Shakeel Hussain <shakeel.hussain@careem.com>
2020-04-25 15:09:20 +0500

    GREEN: added roll and score methods

---
+++ src/main/java/com/meetup/kata/Game.java
@@ -0,0 +1,12 @@
+package com.meetup.kata;
+
+public class Game {
+
+  public void roll(int pins) {
+
+  }
+
+  public int score() {
+    return 8;
+  }
+}
--- src/test/java/com/meetup/kata/BowlingGameShould.java
+++ src/test/java/com/meetup/kata/BowlingGameShould.java
@@ -1,12 +1,21 @@
 package com.meetup.kata;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class BowlingGameShould {
 
   @Test
-  public void doSomething() {
-    assert 1 == 1;
+  public void startGame()
+  {
+    Game game = new Game();
+    int firstRollPins = 6;
+    game.roll(firstRollPins);
+    int secondRollPins = 2;
+    game.roll(secondRollPins);
+
+    Assert.assertEquals(8, game.score());
+
   }
 
 
