433b3dfd17ada1d28f9b1fa2be066acc1451c208
lamak <lamak.qaizar@careem.com>
2020-04-25 23:07:20 +0500

    REFACTOR: applied TPP to move to a simpler implementation using tail recursion (removed all mutation)

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,21 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BowlingGame {
 
-    int[] frames = {0,0,0};
-    int currentFrame = 0;
-    int score = 0;
+    List<Integer> rolls = new ArrayList<>();
 
     public void roll(int pins) {
-        score += pins;
-
-        if (frames[currentFrame] == 10) {
-            score += pins;
-            currentFrame++;
-        }
-
-        frames[currentFrame] += pins;
+        rolls.add(pins);
     }
 
     public int score() {
-        return score;
+        return recursiveScore(rolls, 0, 1);
+    }
+
+    public int recursiveScore(List<Integer> numbers, int previousRoll, int multiplier) {
+        if (numbers.size() > 0) {
+            if (numbers.get(0) + previousRoll == 10) {
+                return multiplier * numbers.get(0)
+                        + recursiveScore(numbers.subList(1, numbers.size()), 0, 2);
+            }
+            return multiplier * numbers.get(0)
+                    + recursiveScore(numbers.subList(1, numbers.size()), numbers.get(0), 1);
+        }
+        return 0;
     }
 }
