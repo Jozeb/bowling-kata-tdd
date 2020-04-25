36f72cbc1af121f31fe1ac0829c8dfe51ee86fb0
Saad Ismail <saad3112@gmail.com>
2020-04-25 16:31:12 +0500

    GREEN implement for strike cases

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,30 +1,54 @@
 public class BowlingGame {
 
     int score;
-    int last_score;
+    int lastScore;
+    int currentScore;
+    boolean isLastFrameStrike;
+    int strikeHandled;
     boolean isSecondRoll;
 
     BowlingGame() {
         score = 0;
-        last_score = 0;
+        lastScore = 0;
+        currentScore = 0;
+        strikeHandled = 0;
         isSecondRoll = false;
+        isLastFrameStrike = false;
     }
 
     public void roll(int rollPins) {
 
-        if (last_score == 10) {
-            this.score = this.score + rollPins;
-        }
-
-        if (isSecondRoll) {
-            last_score = last_score + rollPins;
+        if (isLastFrameStrike) {
+//            strike
+            if (strikeHandled < 2) score = score + rollPins;
+            strikeHandled++;
+            if (strikeHandled == 2) {
+                isLastFrameStrike = false;
+                strikeHandled = 0;
+            }
         } else {
-            last_score = rollPins;
+//            spare
+            if (lastScore == 10) {
+                this.score = this.score + rollPins;
+            }
+
+            if (isSecondRoll) {
+                lastScore = lastScore + rollPins;
+            } else {
+                lastScore = rollPins;
+            }
         }
 
-        isSecondRoll = !isSecondRoll;
+        if (rollPins != 10) {
+            isSecondRoll = !isSecondRoll;
+        }
 
         this.score = this.score + rollPins;
+
+        if (rollPins == 10) {
+            isLastFrameStrike = true;
+            strikeHandled = 0;
+        }
     }
 
     public int score() {
