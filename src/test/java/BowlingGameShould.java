2a63b38183d00c08d54d5566d751f6cfa0000391
lamak <lamak.qaizar@careem.com>
2020-04-25 21:36:33 +0500

    GREEN: added two cases for 0 score and two rolls

--- src/test/java/BowlingGameShould.java
+++ src/test/java/BowlingGameShould.java
@@ -1,11 +1,31 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class BowlingGameShould {
 
-  @Test
-  public void doSomething() {
-    assert 1 == 1;
-  }
+    @Test
+    public void showZeroScoreWhenNoPinsAreDown() {
+        BowlingGame bowlingGame = new BowlingGame();
+        bowlingGame.roll(0);
+        int actual = bowlingGame.score();
+        assertEquals(0, actual);
+    }
 
+    @Test
+    public void showOneScoreWhenOnePinsIsDown() {
+        BowlingGame bowlingGame = new BowlingGame();
+        bowlingGame.roll(1);
+        int actual = bowlingGame.score();
+        assertEquals(1, actual);
+    }
 
+    @Test
+    public void showCorrectScoreAfterTwoRolls() {
+        BowlingGame bowlingGame = new BowlingGame();
+        bowlingGame.roll(1);
+        bowlingGame.roll(1);
+        int actual = bowlingGame.score();
+        assertEquals(2, actual);
+    }
 }
