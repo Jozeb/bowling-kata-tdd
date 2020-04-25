61a4b38c94d229ddd9bec53395a8e10e5cfe760a
Saad Ismail <saad3112@gmail.com>
2020-04-25 15:09:10 +0500

    RED add test for single role of 1

--- src/test/java/BowlingGameShould.java
+++ src/test/java/BowlingGameShould.java
@@ -1,10 +1,16 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class BowlingGameShould {
 
   @Test
-  public void doSomething() {
-    assert 1 == 1;
+  public void haveCumulativeScoreAfterSingleRoleOf1() {
+    BowlingGame bowlingGame = new BowlingGame();
+    bowlingGame.roll(1);
+    int actualScore = bowlingGame.score();
+    int expectedScore = 1;
+    assertEquals(expectedScore, actualScore);
   }
 
 
