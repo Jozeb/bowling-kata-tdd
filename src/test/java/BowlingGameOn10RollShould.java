513d0acaf4dad7ca125d6b5b856187c1fe8ff6d3
somanbaqai <soman.baqai@gmail.com>
2020-04-25 17:33:00 +0500

    RED : spare on 10th roll

--- src/test/java/BowlingGameOn10RollShould.java
+++ src/test/java/BowlingGameOn10RollShould.java
@@ -40,4 +40,40 @@ public class BowlingGameOn10RollShould {
         bowlingGame.roll(1);
 
     }
+    @Test
+    public void throwExceptionWhenSpare(){
+        BowlingGame bowlingGame = new BowlingGame();
+
+        bowlingGame.roll(1);
+        bowlingGame.roll(2);
+
+        bowlingGame.roll(5);
+        bowlingGame.roll(5);
+
+        bowlingGame.roll(10);
+
+        bowlingGame.roll(1);
+        bowlingGame.roll(1);
+
+        bowlingGame.roll(1);
+        bowlingGame.roll(1);
+
+        bowlingGame.roll(1);
+        bowlingGame.roll(1);
+
+        bowlingGame.roll(1);
+        bowlingGame.roll(1);
+
+        bowlingGame.roll(1);
+        bowlingGame.roll(1);
+
+        bowlingGame.roll(1);
+        bowlingGame.roll(1);
+
+        bowlingGame.roll(4);
+        bowlingGame.roll(6);
+
+        bowlingGame.roll(1);
+
+    }
 }
