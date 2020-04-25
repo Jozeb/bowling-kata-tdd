554de69e0b5ca5b568e54e3f856d251fa64dac6a
somanbaqai <soman.baqai@gmail.com>
2020-04-25 15:45:35 +0500

    REFACTOR : removing nested duplicated conditions

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -3,21 +3,21 @@ public class BowlingGame {
     int score;
     int last_score;
     boolean isSecondRoll;
-    BowlingGame(){
-        score=0;
-        last_score=0;
+
+    BowlingGame() {
+        score = 0;
+        last_score = 0;
         isSecondRoll = false;
     }
+
     public void roll(int rollPins) {
 
-        if(last_score == 10){
+        if (last_score == 10) {
             rollPins = rollPins * 2;
         }
-        if(isSecondRoll == true){
-            if(rollPins +last_score == 10){
-                last_score = 10;
-            };
-        }else{
+        if (isSecondRoll) {
+            last_score = last_score + rollPins;
+        } else {
             last_score = rollPins;
         }
 
