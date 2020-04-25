4ba746918ca77e70bd19a087ee3151f31a928afa
lamak <lamak.qaizar@careem.com>
2020-04-25 21:38:58 +0500

    GREEN: added check for previous frame

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,7 +1,16 @@
 public class BowlingGame {
+
+    int previousFrame;
     int score = 0;
+
     public void roll(int pins) {
         score += pins;
+
+        if (previousFrame == 10) {
+            score += pins;
+        }
+
+        previousFrame += pins;
     }
 
     public int score() {
