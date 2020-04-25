88e702d75c4adff6f0d9e2829b1f724347b237c6
Sauban bin Usman <saubanbinusman@yahoo.com>
2020-04-25 15:47:41 +0500

    [REFACTOR] Minor fixes and refactors

--- src/test/java/KnockedPinCountShould.java
+++ src/test/java/KnockedPinCountShould.java
@@ -6,6 +6,13 @@ public class KnockedPinCountShould {
     @Test
     public void BeConstructable() {
         KnockedPinCount knockedPinCount = new KnockedPinCount(5);
+        Assert.assertEquals(5, knockedPinCount.getCount());
+    }
+
+    @Test
+    public void AllowZeroValue() {
+        KnockedPinCount knockedPinCount = new KnockedPinCount(0);
+        Assert.assertEquals(0, knockedPinCount.getCount());
     }
 
     @Test(expected = AssertionError.class)
