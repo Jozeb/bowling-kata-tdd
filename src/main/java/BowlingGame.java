3516cbeb9bee4a1fb2c7c0373e93183b6b6ef7d1
somanbaqai <soman.baqai@gmail.com>
2020-04-25 17:20:19 +0500

    Revert "RED: exception for 11 frame"
    This reverts commit edc06f0d84061b2db24b6c9a28843fdd85f5244e.

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,29 +1,68 @@
 public class BowlingGame {
 
     int score;
-    int last_score;
+    int lastScore;
+    boolean isLastFrameSpare;
+    boolean isLastFrameStrike;
+    int strikesHandled;
     boolean isSecondRoll;
 
     BowlingGame() {
         score = 0;
-        last_score = 0;
+        lastScore = 0;
+        strikesHandled = 0;
         isSecondRoll = false;
+        isLastFrameSpare = false;
+        isLastFrameStrike = false;
     }
 
     public void roll(int rollPins) {
-
-        if (last_score == 10) {
-            rollPins = rollPins * 2;
-        }
-        if (isSecondRoll) {
-            last_score = last_score + rollPins;
-        } else {
-            last_score = rollPins;
+        if (isLastFrameStrike) {
+            handleLastStrikeCase(rollPins);
+        } else if (isLastFrameSpare) {
+            handleLastSpareCase(rollPins);
         }
 
-        isSecondRoll = !isSecondRoll;
+        checkSpareCase(rollPins);
+        checkStrikeCase(rollPins);
 
         this.score = this.score + rollPins;
+        lastScore = rollPins;
+    }
+
+    private void checkStrikeCase(int rollPins) {
+        if (isAllPinsDown(rollPins)) {
+            isLastFrameStrike = true;
+            strikesHandled = 0;
+            isSecondRoll = false;
+        }
+    }
+
+    private boolean isAllPinsDown(int rollPins) {
+        return rollPins == 10;
+    }
+
+    private void checkSpareCase(int rollPins) {
+        if (isSecondRoll) {
+            if (lastScore + rollPins == 10) {
+                isLastFrameSpare = true;
+            }
+        }
+        isSecondRoll = !isSecondRoll;
+    }
+
+    private void handleLastSpareCase(int rollPins) {
+        this.score = this.score + rollPins;
+        isLastFrameSpare = false;
+    }
+
+    private void handleLastStrikeCase(int rollPins) {
+        if (strikesHandled < 2) score = score + rollPins;
+        strikesHandled++;
+        if (strikesHandled == 2) {
+            isLastFrameStrike = false;
+            strikesHandled = 0;
+        }
     }
 
     public int score() {
