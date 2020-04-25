65b3534996644ab7dedd441d52ca9c5ccf467a43
lamak <lamak.qaizar@careem.com>
2020-04-25 23:34:11 +0500

    REFACTOR: extracted methods

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -15,17 +15,33 @@ public class BowlingGame {
 
     public int recursiveScore(List<Integer> numbers, int previousRoll, int multiplier) {
         if (numbers.size() > 0) {
-            if (numbers.get(0) + previousRoll == 10) {
-                return multiplier * numbers.get(0)
-                        + recursiveScore(numbers.subList(1, numbers.size()), 0, 2);
+            if (isSpare(numbers, previousRoll)) {
+                return multiplyCurrentRoll(numbers, multiplier)
+                        + recursiveScore(splitList(numbers), 0, 2);
             }
-            if (previousRoll == 0) {
-                return multiplier * numbers.get(0)
-                        + recursiveScore(numbers.subList(1, numbers.size()), numbers.get(0), 1);
+            if (firstRollOfFrame(previousRoll)) {
+                return multiplyCurrentRoll(numbers, multiplier)
+                        + recursiveScore(splitList(numbers), numbers.get(0), 1);
             }
-            return multiplier * numbers.get(0)
-                    + recursiveScore(numbers.subList(1, numbers.size()), 0, 1);
+            return multiplyCurrentRoll(numbers, multiplier)
+                    + recursiveScore(splitList(numbers), 0, 1);
         }
         return 0;
+    }
+
+    private List<Integer> splitList(List<Integer> numbers) {
+        return numbers.subList(1, numbers.size());
+    }
+
+    private int multiplyCurrentRoll(List<Integer> numbers, int multiplier) {
+        return multiplier * numbers.get(0);
+    }
+
+    private boolean firstRollOfFrame(int previousRoll) {
+        return previousRoll == 0;
+    }
+
+    private boolean isSpare(List<Integer> numbers, int previousRoll) {
+        return numbers.get(0) + previousRoll == 10;
     }
 }
