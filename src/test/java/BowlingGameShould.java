5436c92117332c585b279787e3f215b3b43dde57
shujaat.ali <=shujaat.dev@gmail.com>
2020-04-25 16:45:30 +0500

    [Refactor]:Added before annotation

--- src/test/java/BowlingGameShould.java
+++ src/test/java/BowlingGameShould.java
@@ -1,36 +1,34 @@
 import exceptions.GameException;
 import exceptions.GameHasEndedException;
 import exceptions.InvalidRollException;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameShould {
 
+    Game game;
 
-  @Test
-  public void exist() {
-    Game game = new Game();
-  }
+    @Before
+    public void init() {
+        game = new Game();
+    }
+
+    @Test
+    public void exist() {
+    }
 
     @Test(expected = GameHasEndedException.class)
     public void endAfter20FramesIfThereIsNoBonus() throws GameException {
-        Game game = new Game();
-        for (int i = 0; i < 21; i++){
-          game.roll(1);
+
+        for (int i = 0; i < 21; i++) {
+            game.roll(1);
         }
     }
 
     @Test(expected = InvalidRollException.class)
-    public  void haveValidRoll() throws GameException {
-         Game game = new Game();
-         game.roll(11);
+    public void haveValidRoll() throws GameException {
+        game.roll(11);
     }
-
-
-
-
-
-
-
 
 
 }
