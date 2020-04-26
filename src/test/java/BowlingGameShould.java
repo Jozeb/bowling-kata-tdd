4d3d5a5dc547f0438471c96d8c7a4631506a6b28
lamak <lamak.qaizar@careem.com>
2020-04-26 15:14:00 +0500

    GREEN: 0 score

--- src/test/java/BowlingGameShould.java
+++ src/test/java/BowlingGameShould.java
@@ -1,11 +1,21 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class BowlingGameShould {
 
-  @Test
-  public void doSomething() {
-    assert 1 == 1;
-  }
+    @Test
+    public void showZeroScoreWhenNoPinsAreDown() {
+        BowlingGame bowlingGame = new BowlingGame();
+        roll(bowlingGame, 0);
+        int actual = bowlingGame.score();
+        assertEquals(0, actual);
+    }
 
+    public void roll(BowlingGame bowlingGame, int... pins) {
+        for (int pin : pins) {
+            bowlingGame.roll(pin);
+        }
+    }
 
 }
